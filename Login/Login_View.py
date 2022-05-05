9790cc726530eed82d6189fc85799f6b6360d57a
trhminh <ttmhien03@gmail.com>
2022-05-05 18:08:27 +0700

    update login view

--- Login/Login_View.py
+++ Login/Login_View.py
@@ -1,7 +1,40 @@
-from tkinter import * 
+from tkinter import *
+import tkinter as tk
 
+class Login_View:
 
-class Login_View: 
     def __init__(self):
         self.window = Tk()
-        self.nothing = None
+        self.window.geometry("1080x720")
+        self.window.configure(bg="#ffffff")
+
+        self.canvas = Canvas(self.window, bg="#ffffff", height=720, width=1080,
+                             bd=0, highlightthickness=0, relief="ridge")
+        self.canvas.place(x=0, y=0)
+
+        self.background_img = PhotoImage(file=f"./Images/Login/background.png")
+        self.background = self.canvas.create_image(660.0, 335.0,
+                                                   image=self.background_img)
+
+        self.login_image = PhotoImage(file=f"./Images/Login/login_image.png")
+        self.login_button = Button(image=self.login_image, borderwidth=0,
+                                   highlightthickness=0, relief="flat", bg="#ffffff")
+        self.login_button.place(x=215, y=553, width=150, height=50)
+
+        self.signup_image = PhotoImage(file=f"./Images/Login/signup_image.png")
+        self.signup_button = Button(image=self.signup_image, borderwidth=0,
+                                    highlightthickness=0, relief="flat", bg="#5D5FEF")
+        self.signup_button.place(x=765, y=430, width=150, height=50)
+
+        self.entry_img = PhotoImage(file=f"./Images/Login/img_textBox0.png")
+        self.entry_bg1 = self.canvas.create_image(290.0, 490.0, image=self.entry_img)
+        self.entry_bg2 = self.canvas.create_image(290.0, 390.0, image=self.entry_img)
+
+        self.name_entry = Entry(self.window, bd=0, bg="#c4c4c4", highlightthickness=0)
+        self.name_entry.place(x=165.0, y=465, width=250.0, height=48)
+
+        self.password_entry = Entry(self.window, bd=0, bg="#c4c4c4", highlightthickness=0)
+        self.password_entry.place(x=165.0, y=365, width=250.0, height=48)
+
+# app = Login_View()
+# app.window.mainloop()
